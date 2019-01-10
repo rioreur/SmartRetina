a68a62067414c6726eb065915befd55c1458eeb1
Theo <theo.hipault@gmail.com>
2019-01-10 16:27:14 +0100

    Deleted all the printf

--- Code/hsv.c
+++ Code/hsv.c
@@ -156,54 +156,41 @@ couleurRVB HSVtoRGB(float H, float S, float V)
 	couleurRVB rvb ; 
 		
 	c = V * S ;
-	newH = H / 60 ; 
-	printf("newH = %f", newH);
-	printf("H = %f", H);
-	printf("c: %f\n", c);
+	newH = H / 60 ;
 	x = c * ( 1- (fabs( fmod(newH, 2) - 1  ) ) );
-	printf("x: %f\n", x); 
 	m = V - c ; 
-	printf("m: %f\n", m);
 	if(0 <= H && H < 60)
 	{
-		printf("if 1 \n");
 		newR = c; 
 		newV = x;
 		newB = 0;
 	}
 	else if(60 <= H && H < 120)
 	{
-		printf("if 2 \n");
 		newR = x; 
 		newV = c;
 		newB = 0;
 	}
 	else if(120 <= H && H < 180)
 	{
-		printf("if 3 \n");
 		newR = 0; 
 		newV = c;
 		newB = x;
 	}
 	else if(180 <= H && H < 240)
 	{
-		printf("if 4 \n");
 		newR = 0; 
 		newV = x;
 		newB = c;
-		printf("coucou\n");
-		printf("r:%f, v:%f, b:%f\n", newR, newV, newB);
 	}
 	else if(240 <= H && H < 300)
 	{
-		printf("if 5 \n");
 		newR = x; 
 		newV = 0;
 		newB = c;
 	}
 	else if(300 <= H && H < 360)
 	{
-		printf("if 6 \n");
 		newR = c; 
 		newV = 0;
 		newB = x;
@@ -251,7 +238,6 @@ couleurRVB HSVtoRGB(float H, float S, float V)
 	rvb.r = R; 
 	rvb.v = G;
 	rvb.b = B;
-	printf("r:%f, v:%f, b:%f\n", R, G, B);
 	return rvb;
 }
 
